cfd9992645862eeed1401f96bbd78abf78777c58
Sourav Barui <74421679+im-sourav@users.noreply.github.com>
2021-11-10 15:31:20 +0530

    ss
    ss

---
+++ joystick.js
@@ -0,0 +1,33 @@
+
+let centerTouch = false;
+
+// degree convert to radian
+function toRadian(degree) {input
+    return (degree * Math.PI) / 180;
+  }
+  
+  // radian convert to Degree
+  function toDegree(radian) {
+    return (radian * 180) / Math.PI;
+  }
+
+
+let jx, jy, sjx = window.innerWidth / 2, sjy = window.innerHeight / 2;
+
+// for joystick
+const touchStart = (e) => {
+//   sjx = e.touches[0].clientX;                 
+//   sjy = e.touches[0].clientY;
+  fire = true;
+};
+const joystick = (e) => {
+    jx = e.touches[0].clientX - sjx;
+    jy = e.touches[0].clientY - sjy;
+
+    let xangle = Math.atan2(jy, jx);
+    angle = -(xangle + toDegree(90));
+};
+const touchEnd = (e) => {
+  fire = false;
+}; 
+jyt.style.display = "none"; 
